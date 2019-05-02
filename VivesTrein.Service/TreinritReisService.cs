4337b3dc344f74444a8a6bb5f7db121f27da3b81
LaurentCallewier <laurentcallewier@outlook.com>
2019-05-02 20:46:24 +0200

    Treinrit plaatsen en prijs

---
+++ VivesTrein.Service/TreinritReisService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VivesTrein.Domain.Entities;
+using VivesTrein.Storage;
+
+namespace VivesTrein.Service
+{
+    public class TreinritReisService
+    {
+        private TreinritReisDAO treinritReisDAO;
+
+        public TreinritReisService()
+        {
+            treinritReisDAO = new TreinritReisDAO();
+        }
+
+        public TreinritReis FindTreinritReis(Treinrit treinrit)
+        {
+            return treinritReisDAO.FindTreinritReis(treinrit);
+        }
+    }
+}
---
+++ VivesTrein.Storage/TreinritReisDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VivesTrein.Domain.Entities;
+
+namespace VivesTrein.Storage
+{
+    public class TreinritReisDAO
+    {
+        private readonly vivestrainContext _db;
+
+        public TreinritReisDAO()
+        {
+            _db = new vivestrainContext();
+        }
+
+        public TreinritReis FindTreinritReis(Treinrit treinrit)
+        {
+            return _db.TreinritReis.Where(x => x.Treinrit == treinrit).FirstOrDefault();
+        }
+    }
+}
